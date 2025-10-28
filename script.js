b8d0314f71ab972b6eaf8c3ca3954e1f2d181133
Ngcozela <ngcozelauyolo@gmail.com>
2025-10-28 17:15:06 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function openCollection(num) {
-  alert("Opening Collection " + num + "...");
-  // In the future, you could redirect to another page:
-  // window.location.href = `collection${num}.html`;
-}
