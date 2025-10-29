bb042b6281f8700ed85e25401a49d32f97014b5c
Ngcozela <ngcozelauyolo@gmail.com>
2025-10-29 16:19:37 +0200

    link address routing fixed & changed overflow to scroll

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function openCollection(num) {
   // In the future, you could redirect to another page:
   // window.location.href = `collection${num}.html`;
 }
+function openCollection(id) {
+  const links = {
+    1: "https://mega.nz/folder/yfZ2AaoQ#zTF7PfZWA76b4xEXkzsOFw",
+    2: "https://mega.nz/folder/TfB1yJoK#Syn3y_p-VGQ43D99TPiJ1g",
+    3: "https://mega.nz/folder/CWgAmSpS#Tw4s30Hg6Yiw_BrmSj9zJA",
+    4: "https://mega.nz/folder/DXg3yIrZ#uqXVAUjtjdWuNeYJeBzOrA",
+    5: "https://mega.nz/folder/PKJQEDhK#Dcp7IfThNt1LyZG5yqI-Eg",
+    6: "https://mega.nz/folder/qLJhlZaY#GHokgrQWUwgxrITUvj0yHg",
+    7: "https://mega.nz/folder/6DpTTDYI#I2vX9ITM6g4jdg588M50rQ",
+    8: "https://mega.nz/folder/LbowTBTY#IiMDk54kU6ezraACqWS4NQ",
+    9: "https://mega.nz/folder/iKoXASwR#oN92zfZK7I1ZespC3Hj0Sg",
+    10: "https://mega.nz/folder/rP5kADLJ#0JaCHCcUR7R6TH72EVKB_Q",
+  };
+
+  // Open link for the clicked tab
+  if (links[id]) {
+    window.open(links[id], "_blank");
+  }
+}
