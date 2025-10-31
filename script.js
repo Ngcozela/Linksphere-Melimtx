bd09a3581dc2d4e0a47cb0cf289707d429efa4bf
Ngcozela <ngcozelauyolo@gmail.com>
2025-10-31 13:56:54 +0200

    background colour change

--- script.js
+++ script.js
@@ -3,20 +3,4 @@ function openCollection(num) {
   // In the future, you could redirect to another page:
   // window.location.href = `collection${num}.html`;
 }
-/*Age Verification Pop-Up*/
-document.addEventListener("DOMContentLoaded", () => {
-  const popup = document.getElementById("age-popup");
-  const confirmBtn = document.getElementById("confirm-age");
 
-  // Check if the user has already confirmed age
-  const verified = localStorage.getItem("ageVerified");
-
-  if (!verified) {
-    popup.classList.add("active");
-  }
-
-  confirmBtn.addEventListener("click", () => {
-    localStorage.setItem("ageVerified", "true");
-    popup.classList.remove("active");
-  });
-});
