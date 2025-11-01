87954e5ce21201554d17f5770c20f9a79978b3b8
Ngcozela <ngcozelauyolo@gmail.com>
2025-11-01 19:32:25 +0200

    scroll bar fix

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function openCollection(num) {
 }
 
 console.log("Clicked collection:", num);
+
+/*Scroll bar script*/
+const container = document.querySelector('.collections-container');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+
+let scrollInterval;
+
+// Smooth continuous scroll on hover
+function startScroll(direction) {
+  stopScroll();
+  scrollInterval = setInterval(() => {
+    container.scrollLeft += direction === 'right' ? 10 : -10;
+  }, 15);
+}
+
+function stopScroll() {
+  clearInterval(scrollInterval);
+}
+
+// Event listeners
+leftArrow.addEventListener('mouseenter', () => startScroll('left'));
+rightArrow.addEventListener('mouseenter', () => startScroll('right'));
+leftArrow.addEventListener('mouseleave', stopScroll);
+rightArrow.addEventListener('mouseleave', stopScroll);
