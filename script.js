824e9096f2fee02363b732b799d572a075021603
Ngcozela <ngcozelauyolo@gmail.com>
2025-11-01 22:02:26 +0200

    nkmk

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
 function openCollection(num) {
+  console.log("Clicked collection:", num);
   alert("Opening Collection " + num + "...");
-  // In the future, you could redirect to another page:
   // window.location.href = `collection${num}.html`;
 }
+/*Section scrolling script*/
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.collections-container');
+  const leftArrow = document.querySelector('.left-arrow');
+  const rightArrow = document.querySelector('.right-arrow');
 
-console.log("Clicked collection:", num);
+  let scrollInterval;
 
-/*Scroll bar script*/
-const container = document.querySelector('.collections-container');
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
+  function startScroll(direction) {
+    stopScroll();
+    scrollInterval = setInterval(() => {
+      container.scrollLeft += direction === 'right' ? 8 : -8;
+    }, 10);
+  }
 
-let scrollInterval;
+  function stopScroll() {
+    clearInterval(scrollInterval);
+  }
 
-// Smooth continuous scroll on hover
-function startScroll(direction) {
-  stopScroll();
-  scrollInterval = setInterval(() => {
-    container.scrollLeft += direction === 'right' ? 8 : -8;
-  }, 10); // smaller interval = smoother scroll
-}
-
-function stopScroll() {
-  clearInterval(scrollInterval);
-}
-
-// Arrow hover control
-leftArrow.addEventListener('mouseenter', () => startScroll('left'));
-rightArrow.addEventListener('mouseenter', () => startScroll('right'));
-leftArrow.addEventListener('mouseleave', stopScroll);
-rightArrow.addEventListener('mouseleave', stopScroll);
-
-// Optional: stop scrolling if user clicks anywhere else
-document.addEventListener('mouseleave', stopScroll);
+  leftArrow.addEventListener('mouseenter', () => startScroll('left'));
+  rightArrow.addEventListener('mouseenter', () => startScroll('right'));
+  leftArrow.addEventListener('mouseleave', stopScroll);
+  rightArrow.addEventListener('mouseleave', stopScroll);
+});
