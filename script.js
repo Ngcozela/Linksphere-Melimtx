6fe940b22a2182e457d629b7d311057bae614a60
Linky Mi Linky <ngcozelauyolo@gmail.com>
2025-11-12 21:56:25 +0200

    kjfhrfg

--- script.js
+++ script.js
@@ -186,8 +186,8 @@ if (adBox) {
     atOptions = {
       'key': '1235cadbe8897064a09ba4fc77b33e18',
       'format': 'iframe',
-      'height': 300,
-      'width': 2000,
+      'height': 90,
+      'width': 723,
       'params': {}
     };
   `;
